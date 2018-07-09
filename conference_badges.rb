776e39f63eae52f75018122ac4125f6b88e83fee
Trevor Jameson <tjameson15@gmail.com>
2018-07-09 10:32:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 
 def batch_badge_creator(arr_names)
-  arr_names.collect{|x| `Hello, my name is #{}.`}
+  arr_names.collect{|x| `Hello, my name is #{x}.`}
   return arr_names
 end
